b08d6dd6463788b5119b62cdba62b894a378a41d
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2020-10-07 15:14:36 +0200

    add authentication

--- code/app/src/main/java/com/example/androidgettingstarted/Backend.kt
+++ code/app/src/main/java/com/example/androidgettingstarted/Backend.kt
@@ -1,9 +1,20 @@
 package com.example.androidgettingstarted
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.AuthChannelEventName
+import com.amplifyframework.auth.AuthException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.auth.cognito.AWSCognitoAuthSession
+import com.amplifyframework.auth.result.AuthSessionResult
+import com.amplifyframework.auth.result.AuthSignInResult
 import com.amplifyframework.core.Amplify
+import com.amplifyframework.core.InitializationStatus
+import com.amplifyframework.hub.HubChannel
+import com.amplifyframework.hub.HubEvent
 
 class Backend private constructor() {
 
@@ -15,11 +26,90 @@ class Backend private constructor() {
 
     fun initialize(applicationContext: Context) : Backend {
         try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
             Amplify.configure(applicationContext)
+
             Log.i(TAG, "Initialized Amplify")
         } catch (e: AmplifyException) {
             Log.e(TAG, "Could not initialize Amplify", e)
         }
+
+        Log.i(TAG, "registering hub event")
+
+        // listen to auth event
+        Amplify.Hub.subscribe(HubChannel.AUTH) { hubEvent: HubEvent<*> ->
+
+            when (hubEvent.name) {
+                InitializationStatus.SUCCEEDED.toString() -> {
+                    Log.i(TAG, "Amplify successfully initialized")
+                }
+                InitializationStatus.FAILED.toString() -> {
+                    Log.i(TAG, "Amplify initialization failed")
+                }
+                else -> {
+                    when (AuthChannelEventName.valueOf(hubEvent.name)) {
+                        AuthChannelEventName.SIGNED_IN -> {
+                            updateUserData(true)
+                            Log.i(TAG, "HUB : SIGNED_IN")
+                        }
+                        AuthChannelEventName.SIGNED_OUT -> {
+                            updateUserData(false)
+                            Log.i(TAG, "HUB : SIGNED_OUT")
+                        }
+                        else -> Log.i(TAG, """HUB EVENT:${hubEvent.name}""")
+                    }
+                }
+            }
+        }
+
+        Log.i(TAG, "retrieving session status")
+
+        // is user already authenticated (from a previous execution) ?
+        Amplify.Auth.fetchAuthSession(
+            { result ->
+                Log.i(TAG, result.toString())
+                val cognitoAuthSession = result as AWSCognitoAuthSession
+                // update UI
+                this.updateUserData(cognitoAuthSession.isSignedIn)
+                when (cognitoAuthSession.identityId.type) {
+                    AuthSessionResult.Type.SUCCESS ->  Log.i(TAG, "IdentityId: " + cognitoAuthSession.identityId.value)
+                    AuthSessionResult.Type.FAILURE -> Log.i(TAG, "IdentityId not present because: " + cognitoAuthSession.identityId.error.toString())
+                }
+            },
+            { error -> Log.i(TAG, error.toString()) }
+        )
+
         return this
+    }
+
+    private fun updateUserData(withSignedInStatus : Boolean) {
+        val userData = UserData.shared
+        userData.setSignedIn(withSignedInStatus)
+    }
+
+    fun signOut() {
+        Log.i(TAG, "Initiate Signout Sequence")
+
+        Amplify.Auth.signOut(
+            { Log.i(TAG, "Signed out!") },
+            { error -> Log.e(TAG, error.toString()) }
+        )
+    }
+
+    fun signIn(callingActivity: Activity) {
+        Log.i(TAG, "Initiate Signin Sequence")
+
+        Amplify.Auth.signInWithWebUI(
+            callingActivity,
+            { result: AuthSignInResult ->  Log.i(TAG, result.toString()) },
+            { error: AuthException -> Log.e(TAG, error.toString()) }
+        )
+    }
+
+    fun handleWebUISignInResponse(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d(TAG, "received requestCode : $requestCode and resultCode : $resultCode")
+        if (requestCode == AWSCognitoAuthPlugin.WEB_UI_SIGN_IN_ACTIVITY_CODE) {
+            Amplify.Auth.handleWebUISignInResponse(data)
+        }
     }
 }
--- code/app/src/main/java/com/example/androidgettingstarted/MainActivity.kt
+++ code/app/src/main/java/com/example/androidgettingstarted/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.androidgettingstarted
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -16,8 +17,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val userData = UserData.shared
+
+        userData.isSignedIn.observe(this, Observer<Boolean> { isSignedUp ->
+            // update UI
+            Log.i(TAG, "isSignedIn changed : $isSignedUp")
+
+            if (isSignedUp) {
+                fabAuth.setImageResource(R.drawable.ic_baseline_lock_open)
+            } else {
+                fabAuth.setImageResource(R.drawable.ic_baseline_lock)
+            }
+        })
+
         // prepare our List view and RecyclerView (cells)
         setupRecyclerView(item_list)
+
+        //setup auth button
+        setupAuthButton(userData)
+    }
+
+    // receive the web redirect after authentication
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Backend.shared.handleWebUISignInResponse(requestCode, resultCode, data)
+    }
+
+    //setup auth button
+    private fun setupAuthButton(userData: UserData) {
+
+        // register a click listener
+        fabAuth.setOnClickListener { view ->
+
+            val authButton = view as FloatingActionButton
+
+            if (userData.isSignedIn.value!!) {
+                authButton.setImageResource(R.drawable.ic_baseline_lock_open)
+                Backend.shared.signOut()
+            } else {
+                authButton.setImageResource(R.drawable.ic_baseline_lock_open)
+                Backend.shared.signIn(this)
+            }
+        }
     }
 
     // recycler view is the list of cells
@@ -36,7 +77,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val TAG = "ItemListActivity"
+        private const val TAG = "MainActivity"
     }
 }
 
